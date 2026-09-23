edeba95a12b316e9568dbd1673c12973f9bbf1a8
Logeshwar718 <logeshwar64@gmail.com>
2026-09-23 19:00:00 +0530

    day 2 completed

--- spendgate/permission.py
+++ spendgate/permission.py
@@ -9,9 +9,17 @@ def share_expense_claim(claim_name,user_email):
         read=1
     )
 
-def expense_claim(user):
+def expense_claim_query(user):
     roles=frappe.get_roles(user)
     if "SG Finance Manager" in roles:
         return ""
-
-    if 
+    if "SG Staff" in roles:
+        return f"`tabExpense Claim`.`employee`={frappe.db.escape(user)}"
+    if "SG Department Head" in roles:
+        department=frappe.db.get_value(
+            "Department",
+            {"department_head": user},
+            "name"
+        )
+        return f"`tabExpense Claim`.`department` = {frappe.db.escape(department)}"
+    return "1=0" 
